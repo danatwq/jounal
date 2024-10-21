f6536762f6fd4feced9c311a6c333e1de3dcc0fb
danatwq <dalghamdi27@gmail.com>
2024-10-21 08:32:19 +0300

    Initial Commit

---
+++ journaling/journalingApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalingApp.swift
+//  journaling
+//
+//  Created by Dana Alghamdi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct journalingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ journalingUITests/journalingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  journalingUITestsLaunchTests.swift
+//  journalingUITests
+//
+//  Created by Dana Alghamdi on 18/04/1446 AH.
+//
+
+import XCTest
+
+final class journalingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
