180b94e7a7d77963942140e3cbbd8938a1ac8029
danatwq <dalghamdi27@gmail.com>
2024-10-22 10:00:57 +0300

    i did the first two interface

---
+++ journaling/EmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyState.swift
+//  journaling
+//
+//  Created by Dana Alghamdi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyState()
+}
