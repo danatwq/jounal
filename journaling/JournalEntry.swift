935727f87447fd7776fdeb746f40732ec54dd92f
danatwq <dalghamdi27@gmail.com>
2024-10-28 10:27:19 +0300

    update2

---
+++ journaling/JournalEntry.swift
@@ -0,0 +1,18 @@
+//
+//  JournalEntry.swift
+//  journaling
+//
+//  Created by Dana Alghamdi on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+struct JournalEntry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalEntry()
+}
